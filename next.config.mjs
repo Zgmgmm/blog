3e68b7d03c3367c4532d49972ec6c5bfe2466def
Zgmgmm <1013679885@qq.com>
2024-08-12 14:33:57 +0000

    fix: asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: 'https://zgmgmm.github.io/', // 使用仓库名称作为前缀
 };
 
 export default nextConfig;
