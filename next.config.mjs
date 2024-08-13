40a198a35cb30136dc2d48b660dcbb4ae896b7dc
Zgmgmm <1013679885@qq.com>
2024-08-13 17:12:04 +0000

    fix: output export

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,   // 每秒检查一次文件变更
-        aggregateTimeout: 300,   // 延迟300ms再重新编译
-      };
-    }
-    return config;
-  },
+    output: 'export',
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    webpack: (config, { dev }) => {
+        if (dev) {
+            config.watchOptions = {
+                poll: 1000,   // 每秒检查一次文件变更
+                aggregateTimeout: 300,   // 延迟300ms再重新编译
+            };
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
